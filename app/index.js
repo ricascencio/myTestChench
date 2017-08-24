5552d220f1e84f33e303058169fc029c9dca98d2
Ricardo <ricardo.ascencio@gmail.com>
2017-08-24 15:06:05 +0000

    adding api.ai api

--- app/index.js
+++ app/index.js
@@ -1,13 +1,27 @@
-import Telegraf, { Extra, Markup } from 'telegraf';
-import config from 'config';
+var Telegraf = require("telegraf"),
+    config = require("config");
 
 /** telegram app **/
-const token = process.env.token || config.get('token');
-const URL = process.env.URL || config.get('URL');
+console.log(config);
+const token = config.get('token');
+const URL = config.get('URL');
 const PORT = process.env.PORT || 5000;
-const isDevelopment = process.env.NODE_ENV === 'development';
+//const isDevelopment = process.env.NODE_ENV === 'development';
+
+console.log(URL);
 
 const app = new Telegraf(token);
+
+app.command('fuel', ctx => {
+    console.log("fuelChenchito");
+    ctx.reply("fuelChenchito");
+  });
+  
+app.hears('probando', ctx => {
+    console.log('**BODY**' + ctx.message);
+});
+
+//app.startPolling();
 
 app.telegram.setWebhook(`${URL}/bot${token}`);
 app.startWebhook(`/bot${token}`, null, PORT);
